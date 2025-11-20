501940dbfcfbb575ecba414bd828810373b2b4be
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2025-11-20 20:12:49 +0530

    app.js

---
+++ my-portfolio-app/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import ProjectsSection from './components/ProjectsSection';
+import Footer from './components/Footer';
+
+const Home = () => {
+ return (
+  <div className="bg-white min-h-screen">
+   <Header />
+   <Hero />
+   <ProjectsSection />
+   <Footer />
+  </div>
+ );
+};
+
+function App() {
+ return (
+  <div className="App">
+   <BrowserRouter>
+    <Routes>
+     <Route path="/" element={<Home />} />
+    </Routes>
+   </BrowserRouter>
+  </div>
+ );
+}
+
+export default App;
